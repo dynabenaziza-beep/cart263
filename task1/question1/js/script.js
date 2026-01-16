1e392f82901ebe7eba09128e0e8238267ea9d284
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-01-16 15:13:47 -0500

    Initial commit

---
+++ task1/question1/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function setup() {
+    console.log("go")
+createCanvas (500,500)
+    
+
+
+}
+
+function draw() {
+background("black");
+
+push();
+fill("white");
+ellipse(50, 50,80);
+pop();
+}
