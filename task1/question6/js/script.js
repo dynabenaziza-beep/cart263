74c923697204ab27856c14f922b30060c1c9893f
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-01-18 18:40:49 -0500

    created "text" object  for text settings

--- task1/question6/js/script.js
+++ task1/question6/js/script.js
@@ -1,7 +1,19 @@
 "use strict";
 
-const canvasWidth = 500;
+const canvasWidth = 500; // constant for canvas 
 const canvasHeight = 500;
+
+const text= { 
+ word: "test" ,
+ r: 255,
+ g: 255, 
+ b: 255,
+ size: 28,
+ x: Width/2 ,
+ y: Height /2
+
+
+};
 
 function setup() {
     console.log("go")
