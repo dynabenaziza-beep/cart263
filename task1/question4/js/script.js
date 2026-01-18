a0650726301f8cedbec6287001cee0f0a89d57a2
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-01-18 15:09:01 -0500

    declared variable for canvas width and height

--- task1/question4/js/script.js
+++ task1/question4/js/script.js
@@ -1,10 +1,19 @@
 "use strict";
 
+
+let rectW; // rectangle width 
+let rectH; // rectangle heigth  
+
 function setup() {
     console.log("go")
+    createCanvas(500,500)
 
 }
 
 function draw() {
+ background("black");
 
+
+ 
 }
+
