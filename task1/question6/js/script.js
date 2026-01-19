42d886c23f9d6369fda03d8879f5dab9371d2927
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-01-18 19:06:22 -0500

    add startX and startY for num drawing origin

--- task1/question6/js/script.js
+++ task1/question6/js/script.js
@@ -3,6 +3,8 @@
 const canvasWidth = 500; // constant for canvas 
 const canvasHeight = 500;
 
+
+
 const textData= { 
  word: "test" ,
  r: 255, // white 
@@ -11,9 +13,9 @@ const textData= {
  size: 28,
  x: canvasWidth/2 ,
  y: canvasHeight /2
-
-
 };
+const startX = 50 ;
+const startY = 50; 
 
 function setup() {
     
@@ -22,6 +24,8 @@ createCanvas(canvasWidth,canvasHeight);
 
 function draw() {
 background("black");
+
+
 fill(textData.r, textData.g, textData.b); // text colour for "text" object 
 textSize(text.size); // text size  for "text" object 
 textAlign(CENTER , CENTER);
