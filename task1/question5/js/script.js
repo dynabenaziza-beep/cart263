5b7a8ada809c20d9eec07d2d2f65761c49f73f1c
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-01-18 19:50:51 -0500

    Add orangeSquare object with position, size, and color

--- task1/question5/js/script.js
+++ task1/question5/js/script.js
@@ -6,9 +6,21 @@ const canvasH = 500;
 
 let counter=0;
 
+const orangeSquare = {
+    x: 50, 
+    y: 50,
+    w: 80,
+    h: 80, 
+    color:{
+         r:255,
+        g:150,
+        b: 0
+    }
+};
+
 function setup() {
     console.log("go")
-createCanvas(500,500);
+createCanvas(canvasW, canvasH);
 }
 
 function draw() {
