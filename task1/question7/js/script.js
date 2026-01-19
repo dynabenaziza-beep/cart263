d3aa7aaea4aae102ecb8e3007a8866abc8f79eb7
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-01-18 20:51:15 -0500

    Setup canvas, shape size, color variables, and shape

--- task1/question7/js/script.js
+++ task1/question7/js/script.js
@@ -2,6 +2,16 @@
  const canvasW = 500;
 const canvasH = 500;
 
+let shapeSize = 40; 
+
+// 1 color for all shapes 
+let shapeR;
+let shapeG;
+let shapeB;
+
+// true = cicles , false = squares 
+let drawCircle = true;
+
 function setup() {
     console.log("go")
 createCanvas(500,500);
