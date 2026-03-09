d73fc1f7b25a92e8de4d44013129f88d9f70aaa4
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-03-09 18:34:08 -0400

    create one test dot

--- data/script.js
+++ data/script.js
@@ -1,1 +1,8 @@
 const stage = document.getElementById("stage");
+
+const testDot = document.createElement("div");
+testDot.className = "dot";
+testDot.style.left ="200px"; //set horizontal posiiton of the dot 
+testDot.style.top ="200px" ; //set vertical position of the dot 
+
+stage.appendChild(testDot);
