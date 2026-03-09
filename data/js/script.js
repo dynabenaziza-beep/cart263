c5938385090e51eee7bb35af4b7041176375056e
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-03-09 18:17:31 -0400

    got rid of the odler script.js

--- data/js/script.js
+++
@@ -1,7 +0,0 @@
-const stage = document.getElementById("stage");
-const clearBtn = document.getElementById("clearBtn");
-const countText = document.getElementById("count");
-
-const points = [];
-const maxPoints = 120;
-const typeColors = ["#ffffff", "#9ca3af", "#60a5fa"];
