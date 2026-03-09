761ec369306a83d0ae74aadfc11ca2428eb4772f
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-03-09 18:43:09 -0400

    start ADD dot function

--- data/script.js
+++ data/script.js
@@ -4,3 +4,11 @@ const dots = []; //create an emoty array to store all dots
 const totalDots = 35;//how many dots 
 
 const typeColors = ["#ffffff", "#9ca3af", "#60a5fa"]; // each dot will random use those colors 
+
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+
+  function addDot(x, y) {
+
+}
+}
