49785a7ee747bee4dffddfd827099125a07c455d
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-03-13 16:52:57 -0400

    add check to stop profile from repeating

--- data/script.js
+++ data/script.js
@@ -186,17 +186,8 @@ if (dot.type === 1) grayCount++;   //if dot gray= add 1 to whitecount
 if (dot.type === 2) blueCount++;    //if dot blue= add 1 to whitecount
 
 });
-if (whiteCount > 10 || grayCount > 10 || blueCount > 10) {
-const rect = stage.getBoundingClientRect();
 
-profile.style.left = rect.width / 2 + "px"; // profile horizontally 
-profile.style.top = rect.height / 2 + "px"; //profile vertically 
-
- profile.style.opacity = 0.6; // show the profile
-
-
-} else {
-profile.style.opacity = 0; // hide the profile
+if (profileCreated === false) {
 
 }
 
