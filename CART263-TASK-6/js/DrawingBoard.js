72f1a15fe6f23952ebf25140bec4d0fd3f6ac4fc
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-03-24 14:38:51 -0400

    ifx circle object syntax

--- CART263-TASK-6/js/DrawingBoard.js
+++ CART263-TASK-6/js/DrawingBoard.js
@@ -71,8 +71,8 @@ display: function () {
     this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     this.context.fillStyle = this.color;
     this.context.fill();
-  }
-};
+  },
+
 
     display: function () {
       this.context.beginPath();
