59eba0c7e71d44ae4b9f8077df0b25bfd0a741c0
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-04-14 16:07:05 -0400

    add renderer

--- data/script.js
+++ data/script.js
@@ -253,9 +253,18 @@ animate();
 
 function setupThreeScene() {
 
-  scene = new THREE.Scene();
+ scene = new THREE.Scene();
+
 camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
 camera.position.z = 3;
+
+renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  antialias: true
+});
+
+renderer.setSize(180, 180);
+threeContainer.appendChild(renderer.domElement);
 
 }
 
