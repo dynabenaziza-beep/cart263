c33dad100d43ed679a295a437fbe12df468c009f
dynabenaziza-beep <dynabenaziza@gmail.com>
2026-04-18 14:54:04 -0400

    fixed typo

--- data/script.js
+++ data/script.js
@@ -502,7 +502,7 @@ function getMaxConnectedCount() {
 function groupSimilarDots(){
 for (let i = 0; i < dots.length; i++) {
     for (let j = i + 1; j < dots.length; j++) {
-      const dot1 = dots[j];
+      const dot1 = dots[i];
       const dot2 = dots[j];
 
       const distance = getDistance(dot1, dot2);
